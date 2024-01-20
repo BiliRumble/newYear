b4eb31ebcff541a77022d1bcbcd8c4165206e81d
Rumble <109781840+BiliRumble@users.noreply.github.com>
2024-01-20 05:28:29 +0000

    feat: 日了打不开本地调试

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,18 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <H1>春节倒计时</H1>
+    <timeBox />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup lang="ts">
+import timeBox from './components/timeBox.vue';
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style scoped lang="scss">
+main {
+  display: flex;
+  width: 100vh;
+  height: 100vw;
 }
 </style>
---
+++ src/components/timeBox.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="snow-box">
+		<div class="countdown" data-date="2024/2/10 00:00:00">
+            <div class="countdown-box">
+                <h4 class="countdown-number">00</h4>
+                <h4 class="countdown-title">天</h4>
+            </div>
+            <div class="countdown-box">
+                <h4 class="countdown-number">00</h4>
+                <h4 class="countdown-title">时</h4>
+            </div>
+            <div class="countdown-box">
+                <h4 class="countdown-number">00</h4>
+                <h4 class="countdown-title">分</h4>
+            </div>
+            <div class="countdown-box">
+                <h4 class="countdown-number">00</h4>
+                <h4 class="countdown-title">秒</h4>
+            </div>
+        </div>
+    </div>
+</template>
